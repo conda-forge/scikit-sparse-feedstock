e73b94eb084f944cc0234ad956ce342f7fd28c92
Bernard Roesler <bernard.roesler@gmail.com>
2026-06-04 19:57:29 -0400

    Update recipe/bld.bat to match unix recipe.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,11 @@
+@echo on
+
+REM Make sure setup.py can find the SuiteSparse headers and libraries
 set "SUITESPARSE_INCLUDE_DIR=%LIBRARY_PREFIX%\include\suitesparse"
 set "SUITESPARSE_LIBRARY_DIR=%LIBRARY_PREFIX%\lib"
 
-%PYTHON% -m pip install . --no-deps -vv --ignore-requires-python
+REM Match the Unix recipe compiler/linker hints
+set "CFLAGS=%CFLAGS% -I%LIBRARY_PREFIX%\include -I%LIBRARY_PREFIX%\include\suitesparse"
+set "LDFLAGS=%LDFLAGS% -L%LIBRARY_PREFIX%\lib"
+
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
