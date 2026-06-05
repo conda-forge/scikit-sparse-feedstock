21f04822c576c80840e278850bc14dfd0c114ef7
Bernard Roesler <bernard.roesler@gmail.com>
2026-06-04 21:41:45 -0400

    Update bld.bat file.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ set "SUITESPARSE_INCLUDE_DIR=%LIBRARY_PREFIX%\include\suitesparse"
 set "SUITESPARSE_LIBRARY_DIR=%LIBRARY_PREFIX%\lib"
 
 REM Match the Unix recipe compiler/linker hints
-set "CFLAGS=%CFLAGS% -I%LIBRARY_PREFIX%\include -I%LIBRARY_PREFIX%\include\suitesparse"
-set "LDFLAGS=%LDFLAGS% -L%LIBRARY_PREFIX%\lib"
+set "CL=/I%LIBRARY_PREFIX%\include /I%LIBRARY_PREFIX%\include\suitesparse %CL%"
+set "LINK=/LIBPATH:%LIBRARY_PREFIX%\lib %LINK%"
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
